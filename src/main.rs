d6f9a1aa1f2781f72b2a989d72be9293171d14cd
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-03-28 02:53:35 +0530

    generate random number with external library rand

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+// tells Rust rand is an external dependency, and also does 'use rand'
+extern crate rand;
+
 //use the io library from Rust's std library
 use std::io;
+// use the Rng trait from the rand library
+use rand::Rng;
 
 fn main() {
 
     // print this message with the println macro, NOT function
     println!("Guess the number");
+
+    // generate the random number
+    let secret_num = rand
+                        // get a random number generator local to the current thread of execution and seeded by the OS
+                        ::thread_rng()
+                        // call it's gen_range function which returns a number
+                        .gen_range(1..101);
 
     // declare a mutable string 'guess' with the new function in the String type
     let mut guess = String::new();
@@ -18,5 +30,6 @@ fn main() {
 
     // template formatting into string with placeholders
     println!("You guessed: {}", guess);
+    println!("The secret number was: {}", secret_num);
 
 }
